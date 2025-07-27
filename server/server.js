4ce72b0b07537c06a52229137eab19e4279bd6ef
SanatanBagaria <bagariasanatan@gmail.com>
2025-07-27 12:21:26 +0530

    errorinurl

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
   'https://runners-club-2o8fv04iw-sanatan-bagarias-projects.vercel.app',
   'https://runners-club-n6e4ajxig-sanatan-bagarias-projects.vercel.app',
   'http://localhost:5173',
-  'https://runners-club.vercel.app/', // Optional: keep for local dev
+  'https://runners-club.vercel.app', // Optional: keep for local dev
 ];
 
 app.use(cors({
